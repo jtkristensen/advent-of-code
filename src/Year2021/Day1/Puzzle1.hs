d340904af8504edf157cdacfb0abebe040b13aa3
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2021-12-04 19:12:17 +0100

    more convoluted answer

--- src/Year2021/Day1/Puzzle1.hs
+++ src/Year2021/Day1/Puzzle1.hs
@@ -1,11 +1,7 @@
 module Year2021.Day1.Puzzle1 where
 
 count :: [Int] -> Int
-count [        ] = 0
-count (x : rest) = count' x rest
-  where
-    count' x [        ] = 0
-    count' x (y : rest) = (if y > x then 1 else 0) + count' y rest
+count xs = length $ filter id $ zipWith (<) xs (tail xs)
 
 solution :: [String] -> IO ()
 solution = print . count . map read
