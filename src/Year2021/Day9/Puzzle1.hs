7bbad9f96434e67648b6aa83e9ebd35e548e140d
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2021-12-11 23:50:32 +0100

    day9

---
+++ src/Year2021/Day9/Puzzle1.hs
@@ -0,0 +1,25 @@
+module Year2021.Day9.Puzzle1 where
+
+import AdventLib.Parsing
+import AdventLib.Grids
+
+type HeatMap = Grid Int
+
+heat_map :: Int -> Int -> Parser HeatMap
+heat_map = gridOf (read . return <$> lexeme digit)
+
+low :: HeatMap -> [Point]
+low m =
+  map fst $
+  filter (\(p, ns) -> all (get p m<) ns) $
+  map (\(p, ps) -> (p, map (\q -> get q m) ps)) $
+  map (\p -> (p, adjecent m p)) $
+  points_of m
+
+solution :: String -> IO ()
+solution input =
+  let text   = lines input
+      height = length $ text
+      width  = length $ head text
+  in  print $ sum . (\m -> map ((+1) . flip get m) $ low m) <$>
+      heat_map width height `from` input
---
+++ src/Year2021/Day9/Puzzle2.hs
@@ -0,0 +1,36 @@
+module Year2021.Day9.Puzzle2 where
+
+import AdventLib.Parsing
+import AdventLib.Grids
+
+import Year2021.Day9.Puzzle1
+  (HeatMap, heat_map, low)
+
+import Data.List (sort, nub)
+
+type Basin = [Point]
+
+expand :: HeatMap -> Basin -> Basin
+expand h b =
+  let extra = filter (\p -> not (p `elem` b) && get p h /= 9) $ b >>= adjecent h
+  in  nub $ b ++ extra
+
+basin :: HeatMap -> Point -> Basin
+basin h p = basin' [p]
+  where
+    basin' ps =
+      let ps' = expand h ps
+      in  if ps == ps'
+          then ps
+          else basin' ps'
+
+basin_sizes :: HeatMap -> [Int]
+basin_sizes h = map length $ nub $ map (sort . basin h) (low h)
+
+solution :: String -> IO ()
+solution input =
+  let text   = lines input
+      height = length $ text
+      width  = length $ head text
+      hm     = heat_map width height `from` input
+  in  print $ product . take 3 . reverse . sort . basin_sizes <$> hm
