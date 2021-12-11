c5735db2c238ac9e1389d84b99463afd748c4f91
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2021-12-11 21:32:26 +0100

    cleaner code

--- src/Year2021/Day10/Puzzle1.hs
+++ src/Year2021/Day10/Puzzle1.hs
@@ -1,7 +1,6 @@
 module Year2021.Day10.Puzzle1 where
 
 data Status = OK | Corrupted Char | Incomplete [Char]
-  deriving Show
 
 match :: (Char , Char) -> Bool
 match = (`elem` [('(',')'), ('[',']'), ('{','}'), ('<','>')])
@@ -12,14 +11,12 @@ isOpen = (`elem` ['(', '[', '{', '<'])
 status :: String -> Status
 status = status' []
   where
-  status' [           ] [           ] = OK
-  status' stack@(_ : _) [           ] = Incomplete stack
-  status' stack (c : rest) | isOpen c = status' (c : stack) rest
-  status' []    (c : _   )            = Corrupted c
-  status' (o : stack) (c : rest)      =
-    if   match (o, c)
-    then status' stack rest
-    else Corrupted c
+    status' [           ] [           ]      = OK
+    status' stack@(_ : _) [           ]      = Incomplete stack
+    status' stack (c : rest) | isOpen c      = status' (c : stack) rest
+    status' [           ] (c : _      )      = Corrupted c
+    status' (s:tack) (i:nput) | match (s, i) = status' tack nput
+    status' _          input                 = Corrupted $ head input
 
 syntax_error_score :: Status -> Int
 syntax_error_score (Corrupted ')') =     3
