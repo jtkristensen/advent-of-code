7bf9dec8dd33d13433f3c9214258acbcdd872161
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2021-12-11 21:43:04 +0100

    cleaner code

--- src/Year2021/Day10/Puzzle1.hs
+++ src/Year2021/Day10/Puzzle1.hs
@@ -12,18 +12,18 @@ status :: String -> Status
 status = status' []
   where
     status' [      ] [      ]                = OK
-    status' (s:tack) [      ]                = Incomplete stack
+    status' (s:tack) [      ]                = Incomplete (s:tack)
     status' stack    (i:nput) | isOpen i     = status' (i : stack) nput
     status' [      ] (i:_   )                = Corrupted i
     status' (s:tack) (i:nput) | match (s, i) = status' tack nput
     status' _        (i:_   )                = Corrupted i
 
-syntax_error_score :: Status -> Int
-syntax_error_score (Corrupted ')') =     3
-syntax_error_score (Corrupted ']') =    57
-syntax_error_score (Corrupted '}') =  1197
-syntax_error_score (Corrupted '>') = 25137
-syntax_error_score _               =     0
+score :: Status -> Int
+score (Corrupted ')') =     3
+score (Corrupted ']') =    57
+score (Corrupted '}') =  1197
+score (Corrupted '>') = 25137
+score _               =     0
 
 solution :: String -> IO ()
-solution = print . sum . map (syntax_error_score . status) . lines
+solution = print . sum . map (score . status) . lines
