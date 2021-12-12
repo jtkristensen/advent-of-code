935fa0b071d617771b7d2cb58250954bbee4649e
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2021-12-12 04:38:46 +0100

    day 5

--- src/Year2021/Day5/Puzzle1.hs
+++ src/Year2021/Day5/Puzzle1.hs
@@ -1,6 +1,47 @@
 module Year2021.Day5.Puzzle1 where
 
+import AdventLib.Parsing
+import Data.List ((\\), nub)
 
+type Point   = (Int, Int)
+type Segment = (Point, Point)
+
+segment :: Parser Segment
+segment =
+  do x1 <- number
+     symbol ","
+     y1 <- number
+     symbol "->"
+     x2 <- number
+     symbol ","
+     y2 <- number
+     return ((x1, y1), (x2, y2))
+
+segments :: Parser [Segment]
+segments =
+  do s <- many segment
+     eof
+     return s
+
+horizontal :: Segment -> Bool
+horizontal (p1, p2) = fst p1 == fst p2
+
+vertical :: Segment -> Bool
+vertical (p1, p2) = snd p1 == snd p2
+
+draw :: Segment -> [Point]
+draw s@((x1, y1), (x2, y2)) =
+  case (horizontal s, vertical s) of
+    (True, _) -> [(,)] <*> [x1] <*> [min y1 y2..max y1 y2]
+    (_, True) -> [(,)] <*> [min x1 x2.. max x1 x2] <*> [y1]
+    _         -> []
 
 solution :: String -> IO ()
-solution input = print input
+solution input =
+  print $
+  length .
+  nub .
+  (\ps -> ps \\ nub ps) .
+  concat .
+  map draw <$>
+  segments `from` input
---
+++ src/Year2021/Day5/Puzzle2.hs
@@ -0,0 +1,35 @@
+module Year2021.Day5.Puzzle2 where
+
+import Data.List
+  ( (\\) , nub )
+
+import AdventLib.Parsing
+  ( from )
+
+import Control.Arrow
+  ( first, second )
+
+import Year2021.Day5.Puzzle1
+  ( Point, Segment, segments )
+
+closer :: Int -> Int -> Int
+closer target n =
+  case target `compare` n of
+    LT -> n - 1
+    GT -> n + 1
+    _  -> n
+
+draw :: Segment -> [Point]
+draw (p1, p2) | p1 == p2 = [p1]
+draw (p1, p2)            =
+  p1 : draw (first (closer (fst p2)) $ second (closer (snd p2)) p1, p2)
+
+solution :: String -> IO ()
+solution input =
+  print $
+  length .
+  nub .
+  (\ps -> ps \\ nub ps) .
+  concat .
+  map draw <$>
+  segments `from` input
