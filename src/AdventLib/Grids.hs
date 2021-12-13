f8236f38ae580a5538e56abe659ba2864f31c94a
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2021-12-13 19:03:52 +0100

    better library

--- src/AdventLib/Grids.hs
+++ src/AdventLib/Grids.hs
@@ -3,13 +3,16 @@ module AdventLib.Grids where
 type Grid a = [[a]]
 type Point  = (Int, Int)
 
+fresh :: Int -> Int -> a -> Grid a
+fresh n m a = take m $ (repeat (take n (repeat a)))
+
 get :: Point -> Grid a -> a
-get (0, j) (row :  _) = row !! j
-get (i, j) (_ : rest) = get (i - 1, j) rest
+get (i, 0) (row :  _) = row !! i
+get (i, j) (_ : rest) = get (i, j - 1) rest
 
 put :: a -> Point -> (Grid a -> Grid a)
-put a (0, j) (row : rest) = (take j row ++ a : drop (j + 1) row) : rest
-put a (i, j) (row : rest) = row : put a (i - 1, j) rest
+put a (i, 0) (row : rest) = (take i row ++ a : drop (i + 1) row) : rest
+put a (i, j) (row : rest) = row : put a (i, j - 1) rest
 
 update :: (a -> a) -> Point -> (Grid a -> Grid a)
 update f p g = put (f (get p g)) p g
@@ -21,10 +24,10 @@ update_all :: (a -> a) -> (Grid a -> Grid a)
 update_all f g = updates f (all_points (length g) (length $ head g)) g
 
 x_bound :: Grid a -> Int
-x_bound = length
+x_bound = length . head
 
 y_bound :: Grid a -> Int
-y_bound = length . head
+y_bound = length
 
 neighborhood :: Grid a -> Point -> [Point]
 neighborhood g (i, j) =
