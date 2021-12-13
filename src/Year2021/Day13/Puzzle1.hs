8da249602edae0a58f5622798a6fa04931bcefbe
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2021-12-13 21:06:06 +0100

    code golfing

--- src/Year2021/Day13/Puzzle1.hs
+++ src/Year2021/Day13/Puzzle1.hs
@@ -5,22 +5,17 @@ import AdventLib.Grids
 import Data.List     ( nub   )
 import Control.Arrow ( first , second )
 
-data Axis        = X | Y
-data Instruction = Fold_Along Axis Int
+type Axis        = Char
+type Instruction = [Point] -> [Point]
 
 point :: Parser Point
 point = (,) <$> number <*> (symbol "," >> number)
-
-axis :: Parser Axis
-axis = choice $ [\(s, t) -> symbol s >> return t] <*> [("x", X), ("y", Y)]
 
 instruction :: Parser Instruction
 instruction =
   do symbol "fold"
      symbol "along"
-     x_or_y <- axis
-     symbol "="
-     Fold_Along x_or_y <$> number
+     fold <$> letter <*> (symbol "=" >> number)
 
 transparant :: Parser ([Point], [Instruction])
 transparant = (,) <$> many point <*> many instruction `before` eof
@@ -29,20 +24,17 @@ transpose :: [Point] -> [Point]
 transpose = map $ uncurry $ flip (,)
 
 fold :: Axis -> Int -> [Point] -> [Point]
-fold Y n = transpose . fold X n . transpose
-fold X n = \points -> nub $ left points ++ right points
+fold 'y' n = transpose . fold 'x' n . transpose
+fold _   n = \points -> nub $ left points ++ right points
   where
     left  = filter ((<n) . fst)
     right = map (first $ \x -> n - (x - n)) . filter ((n<) . fst)
 
-perform :: Instruction -> [Point] -> [Point]
-perform (Fold_Along axis n) = fold axis n
-
 solve :: [Point] -> [Instruction] -> [Point]
-solve = foldl (flip perform)
+solve = foldl (flip ($))
 
 solution :: String -> IO ()
 solution input =
-  do print $
-       length . uncurry solve <$> second (return . head) <$>
-       transparant `from` input
+  print $
+  length . uncurry solve <$> second (return . head) <$>
+  transparant `from` input
